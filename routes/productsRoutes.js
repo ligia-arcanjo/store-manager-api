c15594cc0278c5495bd59fc5553dd06d3189fb34
Ligia <ligiaarcanjo84@gmail.com>
2022-06-07 17:43:37 -0300

    feat: configuração inicial dos arquivos de rotas de products e sales

---
+++ routes/productsRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+// criar endpoint GET /products
+
+// criar endpoint GET /products/:id
+
+// criar endpoint POST /products
+
+// criar endpoint PUT /products/:id
+
+// criar endpoint DELETE /products/:id
+
+module.exports = router;
---
+++ routes/salesRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+// criar endpoint GET /sales
+
+// criar endpoint GET /sales/:id
+
+// criar endpoint POST /sales
+
+// criar endpoint PUT /sales/:id
+
+// criar endpoint DELETE /sales/:id
+
+module.exports = router;
