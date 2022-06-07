0ab9763b5bc0cb1b1f1c718fe5b486d17718fc40
Ligia <ligiaarcanjo84@gmail.com>
2022-06-07 17:02:34 -0300

    feat: configuração inicial no app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const productsRoutes = require('./routes/productsRoutes');
+const salesRoutes = require('./routes/salesRoutes');
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/products', productsRoutes);
+app.use('sales', salesRoutes);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
