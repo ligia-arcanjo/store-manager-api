0f883aa9bc2795c3d511c75e3ab589764042ce64
Ligia <ligiaarcanjo84@gmail.com>
2022-06-08 00:31:58 -0300

    fix: alteração na variável de ambiente

--- database/connection.js
+++ database/connection.js
@@ -5,8 +5,8 @@ const connection = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.PORT,
+    database: process.env.MYSQL_DATABASE || 'StoreManager',
+    port: process.env.MYSQL_PORT,
 });
 
 module.exports = connection;
