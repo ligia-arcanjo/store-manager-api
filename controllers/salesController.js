7e9e7b23fa3b79093736f77356d8dcd2efc34817
Ligia <ligiaarcanjo84@gmail.com>
2022-06-08 14:56:01 -0300

    fix: corrigindo tratamento do erro quando não é encontrada sale

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -9,7 +9,7 @@ const getSaleById = async (req, res) => {
   const { id } = req.params;
   const sale = await salesService.getSaleById(id);
 
-  if (!sale) {
+  if (sale.length === 0) {
     return res.status(404).json({ message: 'Sale not found' });
   }
 
