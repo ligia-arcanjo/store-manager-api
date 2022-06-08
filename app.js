98e25ff3f273eb4a0097720ff633c3eeabb75dd6
Ligia <ligiaarcanjo84@gmail.com>
2022-06-08 14:18:03 -0300

    fix: correção na rota /sales no app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/products', productsRoutes);
-app.use('sales', salesRoutes);
+app.use('/sales', salesRoutes);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
