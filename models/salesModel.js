5497cf5b59d9baef8d573f1d759d35c8c4b2b415
Ligia <ligiaarcanjo84@gmail.com>
2022-06-08 14:50:52 -0300

    feat: criada fç para pesquisa de sale pelo id no DB

--- models/salesModel.js
+++ models/salesModel.js
@@ -3,7 +3,10 @@ const connection = require('../database/connection');
 const getAllSales = async () => {
   const [sales] = await connection.execute(
     `SELECT
-    sales.id AS saleId, sales.date, sales_products.product_id AS productId, sales_products.quantity
+      sales.id AS saleId,
+      sales.date,
+      sales_products.product_id AS productId,
+      sales_products.quantity
     FROM sales AS sales
     INNER JOIN sales_products AS sales_products
     ON sales.id = sales_products.sale_id`,
@@ -11,4 +14,19 @@ const getAllSales = async () => {
   return sales;
 };
 
-module.exports = { getAllSales };
+const getSaleById = async (id) => {
+  const [sales] = await connection.execute(
+    `SELECT
+      sales.date,
+      sales_products.product_id AS productId,
+      sales_products.quantity
+    FROM sales AS sales
+    INNER JOIN sales_products AS sales_products
+    ON sales.id = sales_products.sale_id
+    WHERE sales.id = ?`,
+    [id],
+  );
+  return sales;
+};
+
+module.exports = { getAllSales, getSaleById };
