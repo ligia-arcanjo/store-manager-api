78c90d9bd5cd2ae2379cb1c42b3a3659c100b406
Ligia <ligiaarcanjo84@gmail.com>
2022-06-08 14:23:13 -0300

    feat: criada fç para pesquisa das sales do DB

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,7 +1,13 @@
 const connection = require('../database/connection');
 
 const getAllSales = async () => {
-  const [sales] = await connection.execute('SELECT * FROM StoreManager.sales');
+  const [sales] = await connection.execute(
+    `SELECT
+    sales.id AS saleId, sales.date, sales_products.product_id AS productId, sales_products.quantity
+    FROM sales AS sales
+    INNER JOIN sales_products AS sales_products
+    ON sales.id = sales_products.sale_id`,
+  );
   return sales;
 };
 
