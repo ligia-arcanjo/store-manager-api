0f7ce23a1b26869df10301fb3355173e51ab786b
Ligia <ligiaarcanjo84@gmail.com>
2022-10-26 18:43:03 -0300

    retirando comentários

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -4,19 +4,14 @@ const { validateName, validateQuantity } = require('../middlewares/productsValid
 
 const router = express.Router();
 
-// criar endpoint GET /products
 router.get('/', productsController.getAllProducts);
 
-// criar endpoint GET /products/:id
 router.get('/:id', productsController.getProductById);
 
-// criar endpoint POST /products
 router.post('/', validateName, validateQuantity, productsController.addProduct);
 
-// criar endpoint PUT /products/:id
 router.put('/:id', productsController.updateProduct);
 
-// criar endpoint DELETE /products/:id
 router.delete('/:id', productsController.deleteProduct);
 
 module.exports = router;
--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -3,19 +3,14 @@ const salesController = require('../controllers/salesController');
 
 const router = express.Router();
 
-// criar endpoint GET /sales
 router.get('/', salesController.getAllSales);
 
-// criar endpoint GET /sales/:id
 router.get('/:id', salesController.getSaleById);
 
-// criar endpoint POST /sales
 router.post('/', salesController.addSale);
 
-// criar endpoint PUT /sales/:id
 router.put('/:id', salesController.updateSale);
 
-// criar endpoint DELETE /sales/:id
 router.delete('/:id', salesController.deleteSale);
 
 module.exports = router;
